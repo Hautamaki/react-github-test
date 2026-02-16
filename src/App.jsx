b55e22bdb74637fba752474b721a23ed333aaf44
hautamaki <hautamakiras@gmail.com>
2026-02-16 17:40:28 +0200

    complete overhaul

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,32 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+// import { Button } from "/components/base/buttons/button";
+// import { Input } from "/components/base/input/input";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar";
+
+import Home from "./pages/home";
+import Services from "./pages/services";
+import About from "./pages/about";
+import Contact from "./pages/contact";
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-      <p>seemingly the best.</p>
-      <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-    </div>
-  )
+    <BrowserRouter>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
