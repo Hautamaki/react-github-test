74025d20c2cd3c30c5c773f75b0cafe18b7bbe5e
hautamaki <hautamakiras@gmail.com>
2026-02-16 17:45:53 +0200

    link fix

--- src/App.jsx
+++ src/App.jsx
@@ -20,10 +20,10 @@ function App() {
       <Navbar />
 
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="./" element={<Home />} />
+        <Route path="./services" element={<Services />} />
+        <Route path="./about" element={<About />} />
+        <Route path="./contact" element={<Contact />} />
       </Routes>
     </BrowserRouter>
   );
